4e616deb7275f16a2f01bbe91386ae13da976aa6
NikitaBerezhnyj <nikita.berezhnyj@gmail.com>
2026-09-17 21:05:40 +0300

    feat: added base enemy

---
+++ Assets/Scripts/Enemys/Dog.cs
@@ -0,0 +1,14 @@
+public class Dog : Enemy
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
